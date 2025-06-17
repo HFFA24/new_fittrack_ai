2c3fe7e3a1572348b54f7aa3cb4f6c89df21a526
HFFA24 <Fobahelena2@gmail.com>
2025-06-17 10:32:42 +0100

    Add untracked assets, resources, and Firebase service worker

---
+++ fittrack_ai/android/app/src/main/firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+/* firebase-messaging-sw.js */
+importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+});
+
+const messaging = firebase.messaging();
